2912f1e7b69fb0accc35eba4418c674bf4e274e4
L.Colin <987144810@qq.com>
2018-04-12 19:12:27 +0800

    add setting.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+        """store all classes in <alien_invasion>"""
+
+        def __init__(self):
+            """initialize the game setting"""
+            # screen settings
+            self.screen_width = 1200
+            self.screen_height = 650
+            self.bg_color = (230, 230, 230)
