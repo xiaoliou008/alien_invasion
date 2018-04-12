9764a3f021e5a120f512441aefd4861f67184258
L.Colin <987144810@qq.com>
2018-04-12 19:13:25 +0800

    bg_color

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,15 +3,25 @@ import sys
 import pygame
 
 def run_game():
+    # initialize the game and creat a screen object
     pygame.init()
     screen = pygame.display.set_mode((1200, 650))
     pygame.display.set_caption("Alien Invasion")
 
+    # set the backgroung color
+    bg_color = (230, 230, 230)
+
+    # begin the main loop of the game
     while True:
+        # supervise keyboard and mouse event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-            pygame.display.flip()
+        # plot the screen again in every loop
+        screen.fill(bg_color)
+        
+        # make the screen lately plotted visiable
+        pygame.display.flip()
 
 run_game()
